0cdde841d67ddcacd713ad83808e55e2a0ee10f3
AlexSachilovich <AlexSachilovich@gmail.com>
2023-01-15 20:00:02 +0300

    work with java script

---
+++ index.js
@@ -0,0 +1,61 @@
+// const str = 'some RAnDom StreNg'
+// const str2 = 'string 2'
+
+// console.log(str.toLowerCase ()); 
+// console.log(str.toUpperCase ());
+
+// console.log(str.repeat(3))
+
+// console.log(str.includes('Dom'));
+// console.log(str.includes('dom'));
+// console.log(str.toLowerCase().includes('dom'));
+
+// console.log(str.concat(str2));
+
+// console.log(str.startsWith('som'));
+// console.log(str.endsWith('!'));
+
+// console.log(str.replaceAll(' ', ' * '));
+
+// console.log(str.charAt(5));
+
+//  console.log(str.indexOf('m'));
+//  console.log(str.lastIndexOf('m'))
+
+// console.log(str.slice(3, 6));
+// console.log(str.substr(5, 5));
+
+// const gh = prompt()
+// console.log(gh.toUpperCase ());
+
+// const arr = [1,2,3,4, 'gdfs', null, {a:5}, [0,1,2,3,4,5]]
+// console.log(arr[4]);
+
+//  const art = [1,2,3,4,5,6,7,8];
+//  const art1 = [10,11,14,65];
+// console.log(art.concat(art1, 2, 'wert'));
+// const str3 = 'qwerty adfg qert'
+// const arrStr3 = str3.split(' ')
+// console.log(arrStr3); 
+
+
+// const arr01 = [1,2,3,4,5,6]
+// const strArr4 = arr4.join('-*')
+// console.log(strArr4);
+
+// const arr4 = [0,1,2,3,4,5]
+// console.log(arr4.reverse());
+// console.log(arr4.includes(4));
+// arr4.push(6,7,8)
+// console.log(arr4);
+// arr4.pop()
+// console.log(arr4);
+
+// const arr5 = [31,134,446,88,34]
+// console.log(arr5.sort())
+// console.log(arr5.sort((a,b) => a - b))
+
+// console.log(arr4.slice(3, 5));
+// console.log(arr4.slice());
+
+

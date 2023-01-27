f2a42a0b067db223d1129847fc958d7fc39aa10e
Maksemqwe123 <Makcemjgffjh4@gmail.com>
2023-01-27 19:56:01 +0300

    javaScript

---
+++ index.js
@@ -0,0 +1,52 @@
+// console.log('adsfdsfgh');
+
+// console.log('"you"');
+// console.log("'you'");
+
+// const a = 7
+// const b = 9
+// const c = a + b
+// console.log('summ =',a + b);
+// console.log(c);
+
+// console.log();
+
+// const userEnter = prompt()
+// console.log();
+
+// let a = null
+// console.log (a)
+// a = 7
+// console.log(a)
+
+// const obj = {
+//     name: 'Ilya',
+//     age: 16,
+//     job: {
+//         firstjob: 'school' 
+//     }
+// }
+// console.log(typeof );
+
+
+
+
+
+// const num = 5 
+// console.log(typeof num,num);
+
+// const numStr1 = String(num)
+// console.log(typeof numStr1, numStr1);
+
+// const numStr2 = 5 + ''
+// console.log(typeof numStr2, numStr2);
+
+// const numBool = Boolean (num)
+// console.log(typeof numBool, numBool);
+
+// const numBool1 = Boolean (num)
+// console.log(typeof numBool1, numBool1);
+
+// const str = '123'
+// console.log(typeof str, str);
+

867a073ffa340764e137d78d03b90ee9a1c8ddc4
AlexSachilovich <AlexSachilovich@gmail.com>
2023-01-29 19:58:32 +0300

    js4

--- index.js
+++ index.js
@@ -1,61 +1,155 @@
-// const str = 'some RAnDom StreNg'
-// const str2 = 'string 2'
+// const student = {
+//     firstname: 'Ilya',
+//     secondname: 'Kolesnik',
+//     age: 16,
+//     sex: 'man',
+// }
+// const objKeys = Object.keys(student)
+// console.log(objKeys);
 
-// console.log(str.toLowerCase ()); 
-// console.log(str.toUpperCase ());
+// const objValues = Object.values(student)
+// console.log(objValues);
 
-// console.log(str.repeat(3))
-
-// console.log(str.includes('Dom'));
-// console.log(str.includes('dom'));
-// console.log(str.toLowerCase().includes('dom'));
-
-// console.log(str.concat(str2));
-
-// console.log(str.startsWith('som'));
-// console.log(str.endsWith('!'));
-
-// console.log(str.replaceAll(' ', ' * '));
-
-// console.log(str.charAt(5));
-
-//  console.log(str.indexOf('m'));
-//  console.log(str.lastIndexOf('m'))
-
-// console.log(str.slice(3, 6));
-// console.log(str.substr(5, 5));
-
-// const gh = prompt()
-// console.log(gh.toUpperCase ());
-
-// const arr = [1,2,3,4, 'gdfs', null, {a:5}, [0,1,2,3,4,5]]
-// console.log(arr[4]);
-
-//  const art = [1,2,3,4,5,6,7,8];
-//  const art1 = [10,11,14,65];
-// console.log(art.concat(art1, 2, 'wert'));
-// const str3 = 'qwerty adfg qert'
-// const arrStr3 = str3.split(' ')
-// console.log(arrStr3); 
+// console.log(teacher.isMarried);
+// teacher.isMarried = true
+// teacher.job = 'teacher'
+// console.log(teacher);
 
 
-// const arr01 = [1,2,3,4,5,6]
-// const strArr4 = arr4.join('-*')
-// console.log(strArr4);
-
-// const arr4 = [0,1,2,3,4,5]
-// console.log(arr4.reverse());
-// console.log(arr4.includes(4));
-// arr4.push(6,7,8)
-// console.log(arr4);
-// arr4.pop()
-// console.log(arr4);
-
-// const arr5 = [31,134,446,88,34]
-// console.log(arr5.sort())
-// console.log(arr5.sort((a,b) => a - b))
-
-// console.log(arr4.slice(3, 5));
-// console.log(arr4.slice());
 
 
+
+
+// const obj2 = student
+// console.log(obj2)
+// obj2.field = 'ddddd'
+// console.log(student)
+// console.log(obj2);
+
+// const obj3 = {}
+
+// for (prop in student) {
+//     console.log(prop);
+//     obj3 [prop] = student[prop]
+// }
+// console.log(obj3);
+// obj3.lol = 'lol'
+
+// console.log(student);
+// console.log(obj3);
+
+// const obj4 = Object.assign(student)
+// obj4.lol = 'lol'
+// console.log('obj4', obj4);
+// const obj5 = Object.freeze (student)
+// console.log('obj5',obj5);
+
+
+
+// console.log(student.hasOwnProperty('sex'));
+
+
+
+// function hello() {
+//     console.log('hello');
+// }
+
+// hello()
+
+// hello()
+
+// function summ(a,b) {
+//     console.log(a+b);
+// }
+
+// summ(4,56)
+// summ(45,546)
+
+
+
+
+// function summ(a,b,c) {
+//     console.log(a+b-c);
+// }
+
+// summ(4,56,4)
+// summ(2,3,7)
+
+
+
+
+// function summ(a,b,c) {
+//     console.log(a+b-c);
+// }
+
+// summ(4,4,56)
+// summ(7,3,2)
+
+
+// const summ2 = function(a,b,c){
+//     console.log(a+b-c);
+// }
+
+// summ2 (4,4,56)
+// summ (7,3,2)
+
+
+
+
+
+// const summ3 = (a,b,c) => console.log(a+b-c);
+
+// summ3(4,4,56)
+// summ3(7,3,2)
+
+
+
+// function args(a*ax,b*x,c,...rest) {
+//     const sum = a
+//     console.log(arguments); 
+// }
+// args (1,2,3,4,5,6,7,8,9)
+// const arr = [5,4,3,2,1]
+// console.log(...arr);
+
+
+
+// function urvan(a,b,c){
+//     const disk = (b*b-4*a*c)
+//     console.log(disk);
+//     if (disk > 0) {
+//         const x1 = (-b+Math.sqrt(disk))/(2*a)
+//         const x2 = (-b-Math.sqrt(disk))/(2*a)
+//         console.log(x1)
+//         console.log(x2)
+//     } 
+//     else if (disk == 0){
+//             const x = (-b+Math.sqrt(disk))/(2*a)
+//             console.log(x);
+//            } else {
+//                 console.log('нет корней');
+
+//             }
+//         }
+    
+
+
+// urvan(-4,28,-49) 
+
+
+
+function sti(string){
+
+    let counter = 0
+    const arr = [' ', '.', ',', '!']
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === string[i].toUpperCase() && !arr.includes(string[i])) {
+            counter++
+        }
+
+    }
+    console.log(counter);
+}
+
+
+sti ('I have a car')

26cfacc106b69bed6d548d4e93626d9c6c5b1c88
AlexSachilovich <AlexSachilovich@gmail.com>
2023-02-19 19:39:13 +0300

    add

--- index.js
+++ index.js
@@ -1,32 +1,136 @@
-// const b1 = document.getElementById('b1')
-// console.log(b1);
+const divUserName = document.querySelector('.user-name')
+const divGameCount = document.querySelector('.game-count')
+const divGame = document.querySelector('.game')
 
-// const b2_1 = document.querySelector('.b2')
-// const b2_2 = document.querySelector('#b2')
-// console.log(b2_1);
-// console.log(b2_2);
+const inputName = document.getElementById('name')
+
+const gamesAll = document.getElementById('game-all')
+const userWin = document.getElementById('user-win')
+const pcWin = document.getElementById('pc-win')
+
+const btnToCount = document.getElementById('to-count')
+const inputCount = document.getElementById('count')
+const btnToGame = document.getElementById('to-game')
+
+const rockPic = document.getElementById('rock')
+const scissorsPic = document.getElementById('scissors')
+const paperPic = document.getElementById('paper')
+
+const spanMiddleResult = document.querySelector('#middle-result')
+const divMiddleResult = document.querySelector('.middle-result')
+
+btnToCount.setAttribute('disabled', true)
+divGameCount.style.display = 'none'
+divGame.style.display = 'none'
+
+const showCount = () => {
+    divUserName.style.display = 'none'
+    divGameCount.style.display = 'block'
+    inputName.removeEventListener('input', disableName)
+
+}
+
+const disableName = () => {
+    console.log(inputName.value.length);
+    if (inputName.value.length < 2) {
+        btnToCount.setAttribute('disabled', true)
+    } else {
+        btnToCount.removeAttribute('disabled')
+    }
+}
+
+btnToCount.onclick = () => {
+    showCount()
+} 
 
 
-// const li = document.querySelectorAll('li')
-// console.log(li);
+inputName.addEventListener('input', disableName)
 
-// for (let elem of li) {
-//     console.log(elem.matches('li[value="6"]'));
-// }
+btnToGame.onclick = () => {
+    divGameCount.style.display = 'none'
+    divGame.style.display = 'block'
+}
 
-// let chapter = document.querySelector('.chapter')
+const PC_CHOOSE = ['rock', 'scissors', 'paper']
 
-// console.log(chapter.closest('.book'));
-// console.log(chapter.closest('.contents'));
+rockPic.onclick = () => {
+    const user = 'rock'
+    const pc = PC_CHOOSE[Math.floor(Math.random() * 3)]
+    switch (pc) {
+        case 'rock':
+            gameCounter(null, user, pc)
+            break;
+        case 'scissors':
+            gameCounter('user', user, pc)
+            break;
+        case 'paper':
+            gameCounter('pc', user, pc)
+            break;
+    }
+}
+scissorsPic.onclick = () => {
+    const user = 'scissors'
+    const pc = PC_CHOOSE[Math.floor(Math.random() * 3)]
+    switch (pc) {
+        case 'rock':
+            gameCounter('user', user, pc)
+            break;
+        case 'scissors':
+            gameCounter(null, user, pc )
+            break;
+        case 'paper':
+            gameCounter('pc', user, pc)
+            break;
+    }
+}
+paperPic.onclick = () => {
+    const user = 'paper'
+    const pc = PC_CHOOSE[Math.floor(Math.random() * 3)]
+    switch (pc) {
+        case 'rock':
+            gameCounter('user', user, pc)
+            break;
+        case 'scissors':
+            gameCounter('pc', user, pc)
+            break;
+        case 'paper':
+            gameCounter(null, user, pc)
+            break;
+    }
+}
 
-// console.log(chapter.closest('h1'));
 
 
+let countGames = 0
+let pcCounterWin = 0
+let userCounterWin = 0
+let GameCounty = 0
 
-const inpWrite = document.('#writting')
-const p = document.querySelection('#p')
+const gameCounter = (winner, user, pc) => {
+    switch (winner) {
+        case 'user':
+            userCounterWin++
+            break;
 
-inpWrite.addEventListener('input',() => {
-    console.log(inpWrite.value);
-    p.innerHTML = inpWrite.value
-})
+            case 'pc':
+            pcCounterWin++
+            break;
+
+    }
+    
+    countGames++
+    gamesAll.innerHTML = countGames
+    userWin.innerHTML = userCounterWin
+    pcWin.innerHTML = pcCounterWin
+    spanMiddleResult.innerHTML = `ПК выбрал ${pc}, Вы выбрали ${user}`
+    divMiddleResult.style.display = 'block'
+
+    setTimeout(() => {
+        divMiddleResult.style.display = 'none'
+    }, 3000)
+
+    if (countGames === inputCount.value) {
+
+    }
+
+}

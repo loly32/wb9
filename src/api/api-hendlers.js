94ae480b00565449ffd41ce5e91a4bc8cdb472bc
AlexSachilovich <AlexSachilovich@gmail.com>
2023-05-14 19:59:44 +0300

    new

--- src/api/api-hendlers.js
+++
@@ -1,6 +0,0 @@
-import {user} from './mock-data'
-
-export const getUser = async (api) => {
-    return fetch(api)
-        .then(res => res.json())
-}
--- src/api/mock-data.js
+++
@@ -1,5 +0,0 @@
-export const user = {
-    firstname: 'Ilya',
-    secondname: 'Kolesnik',
-    ava: 'https://klike.net/uploads/posts/2020-04/1587719791_1.jpg'
-}
